43efaa94fd691dc5e5ec8208cb47104d0420646e
eventlOwOp <34993620+eventlOwOp@users.noreply.github.com>
2025-05-23 11:48:17 +0800

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,16 +5,6 @@ for modPath in /data/Stardew/Stardew\ Valley/Mods/*/
 do
   mod=$(basename "$modPath")
 
-  # Normalize mod name ot uppercase and only characters, eg. "Always On Server" => ENABLE_ALWAYSONSERVER_MOD
-  var="ENABLE_$(echo "${mod^^}" | tr -cd '[A-Z]')_MOD"
-
-  # Remove the mod if it's not enabled
-  if [ "${!var}" != "true" ]; then
-    echo "Removing ${modPath} (${var}=${!var})"
-    rm -rf "$modPath"
-    continue
-  fi
-
   if [ -f "${modPath}/config.json.template" ]; then
     echo "Configuring ${modPath}config.json"
 
